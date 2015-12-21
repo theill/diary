2b3b22dda7fa0dddce8a599bce4d4b9a645a938e
Peter Theill <peter@theill.com>
2015-12-21 10:06:53 +0100

    Another pointer ref

--- diary.go
+++ diary.go
@@ -273,7 +273,7 @@ func signupPage(w http.ResponseWriter, r *http.Request) {
   ancestorKey := datastore.NewKey(c, "Diary", u.Email, 0, nil)
 
   var diary Diary
-  err := datastore.Get(c, ancestorKey, diary)
+  err := datastore.Get(c, ancestorKey, &diary)
   if err != nil {
     c.Infof("New User found for %s because err %s != null", u.Email, err)
 
